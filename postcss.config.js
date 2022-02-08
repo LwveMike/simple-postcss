95638103862d2ed9363a64823576099275541d4d
Mihai <mplamadeala@noction.com>
2022-02-08 15:59:12 +0200

    Added some packages for PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,18 @@
+const postCssImport = 'postcss-import';
+const cssNano = 'cssnano';
+const autoPrefixer = 'autoprefixer';
+const postCssPresetEnv = 'postcss-preset-env';
+const postCssNormalize = 'postcss-normalize';
+const postCssSimpleVars = 'postcss-simple-vars';
+
+
 module.exports = {
     plugins: [
-        require('postcss-import'),
-        require('cssnano'),
-        require('autoprefixer'),
-        require('postcss-preset-env')({ stage: 1 }),
+        require(postCssImport),
+        require(cssNano),
+        require(autoPrefixer),
+        require(postCssPresetEnv)({ stage: 1 }),
+        require(postCssNormalize),
+        require(postCssSimpleVars)
     ]
 }
